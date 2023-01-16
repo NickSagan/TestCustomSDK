2eec525bc73a588d96fa291ebc49bb77d4c98391
NickSagan <9952728@gmail.com>
2023-01-16 17:24:34 +0100

    check CustomSDK

--- TestCustomSDK/ViewController.swift
+++ TestCustomSDK/ViewController.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import CustomSDK
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print(RandomNumberGenerator.getNumber())
     }
 
 
